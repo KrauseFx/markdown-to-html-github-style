441e4a415d0a578c7a317869d6b96bd4f3f15d95
Felix Krause <krausefx@gmail.com>
2022-12-10 15:31:42 +0100

    Add syntax highlighting
    Via https://github.com/KrauseFx/markdown-to-html-github-style/pull/8 @FarazPatankar

--- convert.js
+++ convert.js
@@ -3,57 +3,93 @@ var fs = require('fs');
 let filename = "README.md"
 let pageTitle = process.argv[2] || ""
 let plausibleDomain = process.argv[3] || ""
+var hljs = require ('highlight.js');
+
+showdown.extension('highlight', function () {
+  function htmlunencode(text) {
+    return (
+      text
+        .replace(/&amp;/g, '&')
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+      );
+  }
+  return [{
+    type: "output",
+    filter: function (text, converter, options) {
+      var left = "<pre><code\\b[^>]*>",
+          right = "</code></pre>",
+          flags = "g";
+      var replacement = function (wholeMatch, match, left, right) {
+        match = htmlunencode(match);
+        var lang = (left.match(/class=\"([^ \"]+)/) || [])[1];
+        left = left.slice(0, 18) + 'hljs ' + left.slice(18);
+        if (lang && hljs.getLanguage(lang)) {
+          return left + hljs.highlight(lang, match).value + right;
+        } else {
+          return left + hljs.highlightAuto(match).value + right;
+        }
+      };
+      return showdown.helper.replaceRecursiveRegExp(text, replacement, left, right, flags);
+    }
+  }];
+});
 
 fs.readFile(__dirname + '/style.css', function (err, styleData) {
-  fs.readFile(process.cwd() + '/' + filename, function (err, data) {
-    if (err) {
-      throw err; 
-    }
-    let text = data.toString();
-
-    converter = new showdown.Converter({
-      ghCompatibleHeaderId: true,
-      simpleLineBreaks: true,
-      ghMentions: true,
-      tables: true
-    });
-
-    var preContent = `
-    <html>
-      <head>
-        <title>` + pageTitle + `</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">`
-
-    if (plausibleDomain.length > 0) {
-      preContent += `
-        <script defer data-domain="` + plausibleDomain + `" src="https://plausible.io/js/script.js"></script>
-      `
-    }
-    preContent += `
-      </head>
-      <body>
-        <div id='content'>
-    `
-
-    let postContent = `
-
-        </div>
-        <style type='text/css'>` + styleData + `</style>
-      </body>
-    </html>`;
-
-    html = preContent + converter.makeHtml(text) + postContent
-
-    converter.setFlavor('github');
-    console.log(html);
-
-    let filePath = process.cwd() + "/README.html";
-    fs.writeFile(filePath, html, { flag: "wx" }, function(err) {
+  fs.readFile(__dirname + '/node_modules/highlight.js/styles/atom-one-dark.css', function(err, highlightingStyles) {
+    fs.readFile(process.cwd() + '/' + filename, function (err, data) {
       if (err) {
-        console.log("File '" + filePath + "' already exists. Aborted!");
-      } else {
-        console.log("Done, saved to " + filePath);
+        throw err; 
       }
+      let text = data.toString();
+
+      converter = new showdown.Converter({
+        ghCompatibleHeaderId: true,
+        simpleLineBreaks: true,
+        ghMentions: true,
+        extensions: ['highlight'],
+        tables: true
+      });
+
+      var preContent = `
+      <html>
+        <head>
+          <title>` + pageTitle + `</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <meta charset="UTF-8">`
+
+      if (plausibleDomain.length > 0) {
+        preContent += `
+          <script defer data-domain="` + plausibleDomain + `" src="https://plausible.io/js/script.js"></script>
+        `
+      }
+      preContent += `
+        </head>
+        <body>
+          <div id='content'>
+      `
+
+      let postContent = `
+
+          </div>
+          <style type='text/css'>` + styleData + `</style>
+          <style type='text/css'>` + highlightingStyles + `</style>
+        </body>
+      </html>`;
+
+      html = preContent + converter.makeHtml(text) + postContent
+
+      converter.setFlavor('github');
+      console.log(html);
+
+      let filePath = process.cwd() + "/README.html";
+      fs.writeFile(filePath, html, { flag: "wx" }, function(err) {
+        if (err) {
+          console.log("File '" + filePath + "' already exists. Aborted!");
+        } else {
+          console.log("Done, saved to " + filePath);
+        }
+      });
     });
   });
 });
